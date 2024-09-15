543726f67ac88940670efe30330ee3df5b6eacf5
jwmathis <jwmathis7@gmail.com>
2024-09-14 20:20:15 -0400

    final code for module 7 exercise 1

--- Coding_Exercises/Module7_Exercise1/Module7_Exercise1.cpp
+++ Coding_Exercises/Module7_Exercise1/Module7_Exercise1.cpp
@@ -6,20 +6,22 @@ int main(void) {
     //=======================================
     // Declaring variables
     int num;
-    int factorial;
+    int factorial = 1;
     // Prompt user for input 
     do {
         cout << "Enter a number to calculate the factorial: ";
         cin >> num;
 
-        // Check if input is negative or a letter
-        if(num < 0 || isalpha(num)) {
-            cout << "Invalid output. ";
+        if(num < 0) {
+            cout << "Invalid output. Please enter a non-negative integer. ";
         }
-    } while (num < 0); // Repeat prompt if invalid
+        else {
+            break;
+        }
+    } while (true); // Repeat prompt if invalid
 
-    for (int i = num; i != 0; i--) {
-        factorial *= num;
+    for (int i = 1; i <= num; i++) {
+        factorial *= i;
     }
 
     cout << "The factorial of " << num << " is " << factorial;
