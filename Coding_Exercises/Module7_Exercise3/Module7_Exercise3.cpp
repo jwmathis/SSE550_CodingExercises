98a18d6ab8858f690e836389c52dcb3e9ef6b382
jwmathis <jwmathis7@gmail.com>
2024-09-16 18:28:57 -0400

    final code for module7 exercise3

---
+++ Coding_Exercises/Module7_Exercise3/Module7_Exercise3.cpp
@@ -0,0 +1,41 @@
+// Module7_Exercise3.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    //=============================================
+    // Declare variables
+    int randomNumber = rand() % 100 + 1;
+    int guess;
+    int attempts = 0;
+    const int maxAttempts = 10;
+    //=============================================
+    cout << "I'm thinkning of a number between 1 and 100. Can you guess it?\n";
+
+    do {
+        cout << "Enter your guess: ";
+        cin >> guess;
+        attempts++;
+
+        if (guess > randomNumber) {
+            cout << "Too high. Try again.\n";
+        }
+        else if (guess < randomNumber) {
+            cout << "Too low. Try again.\n";
+        }
+        else {
+            cout << "Congratulations! You guess the number.\n";
+            break;
+        }
+    } while (attempts < maxAttempts);
+
+    if (attempts == maxAttempts && guess != randomNumber) {
+        cout << "You've exceeded the maximum number of guesses. The number was " << randomNumber;
+    }
+}
+
